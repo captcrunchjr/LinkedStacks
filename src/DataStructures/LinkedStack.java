096bc49bc8b4bd1f690e0cc4dad0d6cdeddfad75
Coding Rooms <git@codingrooms.io>
2023-03-22 17:04:21 +0000

    initial template

--- src/DataStructures/LinkedStack.java
+++ src/DataStructures/LinkedStack.java
@@ -3,6 +3,6 @@ package DataStructures;
 import Exceptions.*;
 import ADTs.*;
 
-public class LinkedList<T extends Comparable<T>> implements ListADT<T> {
+public class LinkedStack<T extends Comparable<T>> implements StackADT<T> {
 
 }
